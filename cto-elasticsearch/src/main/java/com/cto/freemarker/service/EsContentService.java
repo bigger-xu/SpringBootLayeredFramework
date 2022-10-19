ff03cdef953056e8c56db44ec66ace833cd1df23
Mr.Zhang <zhang.yongwei@weitaige.com>
2022-10-19 11:42:02 +0800

    ES检索优化

--- cto-elasticsearch/src/main/java/com/cto/freemarker/service/EsContentService.java
+++ cto-elasticsearch/src/main/java/com/cto/freemarker/service/EsContentService.java
@@ -1,6 +1,13 @@
 package com.cto.freemarker.service;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import com.cto.freemarker.dao.ContentEsRepository;
 import com.cto.freemarker.entity.EsContent;
 import com.cto.freemarker.utils.Page;
@@ -14,24 +21,21 @@ import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.index.query.QueryBuilders.multiMatchQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
@@ -96,7 +100,7 @@ public class EsContentService {
         searchQuery.setPageable(pageable);
 
         // 高亮字段
-       /* AggregatedPage<EsContent> ideas = elasticsearchRestTemplate.queryForPage(searchQuery, EsContent.class, new SearchResultMapper() {
+        AggregatedPage<EsContent> ideas = elasticsearchRestTemplate.queryForPage(searchQuery, EsContent.class, new SearchResultMapper() {
             @Override
             public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
                 List<EsContent> EsContentList = new ArrayList<>();
@@ -141,7 +145,7 @@ public class EsContentService {
                 }
                 return (T) EsContent;
             }
-        });*/
+        });
         return null;
     }
 
